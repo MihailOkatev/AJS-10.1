97c34179ac8bee23b95dd8714a289112691ca6bc
Astario <mokatev@yandex.ru>
2020-06-20 18:08:57 +0300

    linter errors corrected

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,8 +1,10 @@
 import read from './reader';
 import json from './parser';
 
+// eslint-disable-next-line import/prefer-default-export
 export class GameSavingLoader {
   static load() {
+    // eslint-disable-next-line no-unused-vars
     return new Promise((resolve, reject) => {
       const data = read();
       data.then((answer) => {
--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ import { GameSavingLoader } from './GameSavingLoader';
 // eslint-disable-next-line no-unused-vars
 GameSavingLoader.load().then((saving) => {
   console.log(saving);
+// eslint-disable-next-line no-unused-vars
 }, (error) => {
   // ...
 });
